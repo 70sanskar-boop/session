86184ca71c333056e5b602efbf98b6664a846e73
70sanskar-boop <70sanskar@gmail.com>
2025-08-01 12:22:29 +0530

    Create Addition.cpp
    add

---
+++ Addition.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+    cout<<"Addittionof 2 numbers is = "<<a+b<<endl;
+ 
+    
+    
+ return 0;   
+}
